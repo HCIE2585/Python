a1ce1792117d77951aeb703ae83bfc2b6a0555bc
HCIE2585 <18091783025@163.com>
2021-05-27 14:59:53 +0800

    2021052720210527202105272021052720210527202105272021052720210527202105272021052720210527202105272021052720210527202105272021052720210527202105272021052720210527

--- Netmiko/Netmiko_lab1.py
+++ Netmiko/Netmiko_lab1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
-import re
+import os
 import time
 import sys
+import json
 from netmiko import ConnectHandler
+from pprint import pprint
 LogTime = time.strftime('%Y-%m-%d_%H-%M-%S')
 fw = {
         'device_type': 'hp_comware',
@@ -11,10 +13,30 @@ fw = {
         'username': 'banner',
         'password': 'Ops1@12345',
  }
-connect=ConnectHandler(**fw)
-# config_commands=['screen-length disable','display cur']
-output=connect.send_command('dis cur')
-print(output)
-log = open(fw['host'] + '-' + LogTime + '.txt', 'w')  # 定义个log文本文件，
-log.write(output) # 把info信息写到log中，
-log.close()  # 关闭log文件
+connect = ConnectHandler(**fw)
+output = connect.send_command('dis ip routing-table ',use_textfsm=True)
+pprint(output)
+print (json.dumps(output, indent=2))
+log = open(fw['host'] + '-' + LogTime + '.txt', 'w')
+# for output_dict in output:
+    # for key, value in output.items
+    #     log.write(str(value))
+# for content in output:
+#     log.write(content)
+log.write(json.dumps(output, indent=2))
+for item in output:
+    print(item["network"])
+print (len(output))
+log.close()
+
+
+xmr.f2pool.com
+task4.wyptwilight.cn
+dd.avrenren.com
+donate.v2.xmrig.com
+pop.consultinginc.ru
+upgrades.ddns.net
+2xxxxxx.com
+jincc.cc
+bj2.haoxiw.com
+emergency.googel-dns.com
