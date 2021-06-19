b6e605e63dcd74f20124c7e0e71a56323c3e742a
HCIE2585 <18091783025@163.com>
2021-06-19 18:56:38 +0800

    2021061920210619

--- Netmiko/Netmiko_lab3.py
+++ Netmiko/Netmiko_lab3.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
-import os                                       # 因为使用了ntc模版，并且设置了ntc模版的系统变量，调用ntc模版
+# import os                                       # 因为使用了ntc模版，并且设置了ntc模版的系统变量，调用ntc模版
 import time
-import sys
 import json
 from netmiko import ConnectHandler
-from pprint import pprint                       # 为了配合json使输出显示更加美观，pretty print
+# from pprint import pprint                       # 为了配合json使输出显示更加美观，pretty print
 LogTime = time.strftime('%Y-%m-%d_%H-%M-%S')
 fw = {
         'device_type': 'hp_comware',
@@ -19,18 +18,18 @@ print("开始执行命令")
 cmd = ['undo ip route-static 1.1.1.1 32']
 out =  connect.send_config_set(cmd)
 print(out)
-output1 = connect.send_command('display ip interface',use_textfsm=True)       #通过use_textfsm=Ture,调用textfsm，textfsm输出结果是list
+output1 = connect.send_command('display ip interface',use_textfsm=True)       # 通过use_textfsm=Ture,调用textfsm，textfsm输出结果是list
 output2 = connect.send_command('display ip routing-table',use_textfsm=True)
 log = open(fw['host'] + '-' + LogTime + '.txt', 'w')
-log.write(json.dumps(output1, indent=2)+json.dumps(output2, indent=2))        #dumps()可以将输出结果转换成json格式的字符串。
+log.write(json.dumps(output1, indent=2)+json.dumps(output2, indent=2))        # dumps()可以将输出结果转换成json格式的字符串。
 log.close()
 print('设备中UP的接口如下：')
 for interface in output1:
-        f = interface.get('line_status')                                      #使用dict的get()方法获取key对应的vaules
-        if f != 'DOWN':                                                       #对vaules进行判断
-                print(interface.get('intf'))                                  #打印接口名称
-                print('-link-status:',interface.get('line_status'))           #打印链路状态
-                print('-ip_address:',interface.get('ipaddr'))                 #打印设备IP地址
+        f = interface.get('line_status')                                      # 使用dict的get()方法获取key对应的vaules
+        if f != 'DOWN':                                                       # 对vaules进行判断
+                print(interface.get('intf'))                                  # 打印接口名称
+                print('-link-status:',interface.get('line_status'))           # 打印链路状态
+                print('-ip_address:',interface.get('ipaddr'))                 # 打印设备IP地址
 net_count,static_count,direct_count,ospf_count=0,0,0,0
 for net in output2:
         f = net.get('protocal')                                               # 判断protocal对应的vaule
--- Netmiko/READEME.md
+++ Netmiko/READEME.md
@@ -10,3 +10,7 @@
             send_config_from_file()
     lab3
         展示textfsm模块的使用，配合ntc模块，可以将输入内容转换为json格式，对json格式内容进加工，得到想要的数据
+    lab4
+        通过netmiko登陆多台设备进行配置和信息查询，设备除ip地址外，其他登陆参数保持不变
+    lab5
+        通过netmiko登陆多台设备，使用json数据格式，存储需要登陆的设备信息
